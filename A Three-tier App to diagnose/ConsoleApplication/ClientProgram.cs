1960e690c56f05717d3ed6f85055bfdf409821df
marco-dorantes <marcod@gmail.com>
2018-02-08 07:04:51 -0600

    Updated ClientProgram

--- A Three-tier App to diagnose/ConsoleApplication/ClientProgram.cs
+++ A Three-tier App to diagnose/ConsoleApplication/ClientProgram.cs
@@ -24,7 +24,7 @@ namespace ConsoleApplication
     }
     static void Main(string[] args)
     {
-      for (int k = 0; k < 2049*5; ++k) run(k);
+      for (int k = 0; k < 2049 * 5; ++k) run(k);
     }
   }
 }
